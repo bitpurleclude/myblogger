e1d76f3184f219067efb61fc34cbb33dd56b896d
bitpurleclude <1551758442@qq.com>
2024-02-05 15:08:55 +0800

    <new project>

---
+++ src/main/java/com/example/myblogger/MybloggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myblogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybloggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybloggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myblogger/MybloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myblogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
